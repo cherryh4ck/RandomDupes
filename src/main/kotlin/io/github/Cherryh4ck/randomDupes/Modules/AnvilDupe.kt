4fb1a8ddf31ae7e86a73f2281ff14f9934cd70e3
Cherryh4ck <tomas1234567891016@gmail.com>
2026-05-18 16:09:56 -0300

    add 1.17 anvil dupe

---
+++ src/main/kotlin/io/github/Cherryh4ck/randomDupes/Modules/AnvilDupe.kt
@@ -0,0 +1,34 @@
+package io.github.Cherryh4ck.randomDupes.Modules
+
+import io.github.Cherryh4ck.randomDupes.RandomDupes
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.AnvilInventory
+
+class AnvilDupe(private val plugin : RandomDupes) : Listener {
+    @EventHandler
+    fun onAnvilUse(e: InventoryClickEvent) {
+        val inv = e.clickedInventory as? AnvilInventory ?: return
+        val item = e.currentItem ?: return
+        if (e.rawSlot != 2) return
+
+        val p = e.whoClicked as? Player ?: return
+        val loc = inv.location ?: return
+        if (p.inventory.firstEmpty() != -1) return
+
+        val dropItem = Runnable {
+            if (loc.block.type == Material.AIR) {
+                val dupedItem = item.clone()
+                dupedItem.amount = item.amount
+                val dropped = p.world.dropItem(p.location, dupedItem)
+                dropped.velocity = p.eyeLocation.direction
+            }
+        }
+
+        Bukkit.getScheduler().runTaskLater(plugin, dropItem, 1L)
+    }
+}
--- src/main/kotlin/io/github/Cherryh4ck/randomDupes/RandomDupes.kt
+++ src/main/kotlin/io/github/Cherryh4ck/randomDupes/RandomDupes.kt
@@ -1,5 +1,6 @@
 package io.github.Cherryh4ck.randomDupes
 
+import io.github.Cherryh4ck.randomDupes.Modules.AnvilDupe
 import io.github.Cherryh4ck.randomDupes.Modules.DonkeyDeathDupe
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -7,6 +8,7 @@ class RandomDupes : JavaPlugin() {
 
     override fun onEnable() {
         server.pluginManager.registerEvents(DonkeyDeathDupe(), this)
+        server.pluginManager.registerEvents(AnvilDupe(this), this)
     }
 
     override fun onDisable() {
