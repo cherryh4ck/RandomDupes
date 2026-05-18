98802b0eed06708cc39ef4a2a17e7771af10c178
Cherryh4ck <tomas1234567891016@gmail.com>
2026-05-18 02:33:42 -0300

    Initial commit

---
+++ src/main/kotlin/io/github/Cherryh4ck/randomDupes/RandomDupes.kt
@@ -0,0 +1,14 @@
+package io.github.Cherryh4ck.randomDupes
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RandomDupes : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
