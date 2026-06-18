a45e8c067254c58dc2173db9101abfaa6a35790b
Cherryh4ck <tomas1234567891016@gmail.com>
2026-06-18 14:55:05 -0300

    add unfinished dupe command

---
+++ src/main/kotlin/io/github/Cherryh4ck/randomDupes/Commands/DupeCommand.kt
@@ -0,0 +1,26 @@
+package io.github.Cherryh4ck.randomDupes.Commands
+
+import io.github.Cherryh4ck.randomDupes.RandomDupes
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.command.TabExecutor
+import org.bukkit.entity.Player
+
+class DupeCommand(private val plugin : RandomDupes) : TabExecutor {
+    val mm = MiniMessage.miniMessage()
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (sender is Player){
+            val player = sender
+        }
+        else{
+            sender.sendMessage(mm.deserialize("${plugin.prefix} <red>You must be a player to use this command."))
+        }
+        return true
+    }
+
+    override fun onTabComplete(p0: CommandSender, p1: Command, p2: String, p3: Array<out String> ): List<String?> {
+        return emptyList()
+    }
+}
